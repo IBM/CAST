6ead59f48b6b7ecf2b22157a62ebad28e76075f2
David Hermsmeier <dlherms@us.ibm.com>
2018-05-25 07:53:47 -0400

    bb: Null terminate transfer key buffer when retrieving transfer keys

--- bb/src/HandleFile.cc
+++ bb/src/HandleFile.cc
@@ -70,6 +70,7 @@ int HandleFile::getTransferKeys(const uint64_t pJobId, const uint64_t pHandle, u
         }
     }
 
+    pBuffer[l_TransferKeys.length()] = '\0';
     pBufferSize = l_TransferKeys.length();
 
     return rc;
@@ -493,7 +494,8 @@ int HandleFile::get_xbbServerHandleTransferKeys(string& pTransferKeys, const uin
                                 rc = loadHandleFile(l_HandleFile, handlefile.string().c_str());
                                 if (!rc)
                                 {
-                                    pTransferKeys = l_HandleFile->transferKeys;
+                                    // The string is terminated with a line feed...  Don't copy the last character...
+                                    pTransferKeys = (l_HandleFile->transferKeys).substr(0,((l_HandleFile->transferKeys).length()-1));
                                 }
                                 else
                                 {
@@ -1171,17 +1173,17 @@ int HandleFile::update_xbbServerHandleStatus(const LVKey* pLVKey, const uint64_t
                                 if (ce->second.flags & BBTD_Stopped)
                                 {
                                     l_StoppedDefinitions = true;
-                                    LOG(bb,debug) << "update_xbbServerHandleStatus(): Contribid " << ce->first << " stopped";
+                                    LOG(bb,info) << "update_xbbServerHandleStatus(): Contribid " << ce->first << " stopped";
                                 }
                                 if (ce->second.flags & BBTD_Failed)
                                 {
                                     l_FailedDefinitions = true;
-                                    LOG(bb,debug) << "update_xbbServerHandleStatus(): Contribid " << ce->first << " failed";
+                                    LOG(bb,info) << "update_xbbServerHandleStatus(): Contribid " << ce->first << " failed";
                                 }
                                 if (ce->second.flags & BBTD_Canceled)
                                 {
                                     l_CanceledDefinitions = true;
-                                    LOG(bb,debug) << "update_xbbServerHandleStatus(): Contribid " << ce->first << " canceled";
+                                    LOG(bb,info) << "update_xbbServerHandleStatus(): Contribid " << ce->first << " canceled";
                                 }
                             }
                         }
