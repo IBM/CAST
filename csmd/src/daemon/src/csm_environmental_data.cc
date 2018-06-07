570d9a56f796d7a971c76e7e33c3a2ed455909d9
besawn <38794505+besawn@users.noreply.github.com>
2018-06-07 16:58:22 -0400

    CSM ENV: first pass at adding GPU data to CSM_Environmental_Data::Get_Json_String() output

--- csmd/src/daemon/src/csm_environmental_data.cc
+++ csmd/src/daemon/src/csm_environmental_data.cc
@@ -14,6 +14,11 @@
 ================================================================================*/
 
 #include "include/csm_environmental_data.h"
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <sstream>
+
+#include <sys/time.h>
 
 CSM_Environmental_Data::CSM_Environmental_Data()
 {
@@ -155,7 +160,7 @@ bool CSM_Environmental_Data::Set_Labels( const CSM_Environmental_Data& in )
   {
     if( in._Data_Mask.test( GPU_DOUBLE_LABEL_BIT ) )
     {
-      this->Set_Data( in._GPU_Double_Label_Data );
+      Set_Data( in._GPU_Double_Label_Data );
     }
     else
     {
@@ -167,7 +172,7 @@ bool CSM_Environmental_Data::Set_Labels( const CSM_Environmental_Data& in )
   {
     if( in._Data_Mask.test( GPU_LONG_LABEL_BIT ) )
     {
-      this->Set_Data( in._GPU_Long_Label_Data );
+      Set_Data( in._GPU_Long_Label_Data );
     }
     else
     {
@@ -180,7 +185,126 @@ bool CSM_Environmental_Data::Set_Labels( const CSM_Environmental_Data& in )
 
 std::string CSM_Environmental_Data::Get_Json_String()
 {
-  return "Test string";
+  boost::property_tree::ptree node_pt;
+  std::ostringstream oss;
+  
+  std::string json("");
+
+  // This function will return a series of json documents in a single string
+  // Each json document has a set of common node level fields followed by fields specific to 
+  // the type of environmental data being collected
+  // Build the common node level fields first, to be used in each of the individual json documents
+  #define CSM_ENV_DATA_KEY_TIME_STAMP "time_stamp"
+  #define CSM_ENV_DATA_KEY_NODE "node"
+  #define CSM_ENV_DATA_KEY_TYPE "type"
+  
+  #define CSM_ENV_DATA_TYPE_GPU "GPU"
+
+  // Generate a time_stamp
+  char time_stamp_buffer[80];
+  char time_stamp_with_usec[80];
+
+  struct timeval now_tv;
+  time_t rawtime;
+  struct tm *info;
+
+  gettimeofday(&now_tv, NULL);
+  rawtime = now_tv.tv_sec;
+  info = localtime( &rawtime );
+
+  strftime(time_stamp_buffer, 80, "%Y-%m-%d %H:%M:%S", info);
+  snprintf(time_stamp_with_usec, 80, "%s.%06lu", time_stamp_buffer, now_tv.tv_usec);
+  
+  std::string node("testnode01"); 
+  
+  node_pt.put(CSM_ENV_DATA_KEY_TIME_STAMP, time_stamp_with_usec);   
+  node_pt.put(CSM_ENV_DATA_KEY_NODE, node);   
+
+  // Create any GPU json documents 
+  if ( _Data_Mask.test(GPU_DOUBLE_DATA_BIT) || _Data_Mask.test(GPU_LONG_DATA_BIT) )
+  {
+    std::vector<double> gpu_double_data;
+    std::vector<std::string> gpu_double_labels;
+    
+    std::vector<long> gpu_long_data;
+    std::vector<std::string> gpu_long_labels;
+    
+    if ( _Data_Mask.test(GPU_DOUBLE_DATA_BIT) )
+    {
+      gpu_double_data = _GPU_Double_Data.Return_Double_DCGM_Vector();
+      gpu_double_labels = _GPU_Double_Label_Data.Return_Double_DCGM_Field_String_Identifiers();
+    }  
+    
+    if ( _Data_Mask.test(GPU_LONG_DATA_BIT) )
+    {
+      gpu_long_data = _GPU_Long_Data.Return_Long_DCGM_Vector();
+      gpu_long_labels = _GPU_Long_Label_Data.Return_Long_DCGM_Field_String_Identifiers();
+    }
+
+    // Data for each GPU has to be created from pieces stored in four different areas
+    // Do one big consistency check here before proceeding
+    // For both double data and long data, we should have N times as many entries in the data vector as the label vector
+    // where N is the number of GPUs on the node. We then need to reassemble the data into N json documents, one for each GPU.
+    // We need to make sure that both the long data and double data both contains data for an equal number of GPUs.
+    // We also need to support the case where there may only be long data or only double data.
+    uint32_t gpu_double_count(0);
+    uint32_t gpu_long_count(0);
+    
+    if ( (gpu_double_labels.size() > 0) && (gpu_double_data.size() % gpu_double_labels.size() == 0) )
+    {
+      gpu_double_count = (gpu_double_data.size() / gpu_double_labels.size());
+    }
+    
+    if ( (gpu_long_labels.size() > 0) && (gpu_long_data.size() % gpu_long_labels.size() == 0) )
+    {
+      gpu_long_count = (gpu_long_data.size() / gpu_long_labels.size());
+    }
+
+    if ( ( gpu_double_count == 0 && gpu_long_count == 0 ) ||
+         ( gpu_double_count > 0 && gpu_long_count > 0 && gpu_double_count != gpu_long_count ) ) 
+    {
+        LOG(csmenv, error) << "Inconsistent GPU data detected: gpu_double_data.size() = " << gpu_double_data.size() 
+                           << ", gpu_double_labels.size() = " << gpu_double_labels.size()
+                           << ", gpu_long_data.size() = " << gpu_long_data.size()
+                           << ", gpu_long_labels.size() = " << gpu_long_labels.size();
+    } 
+    else
+    {
+      uint32_t gpu_count(gpu_long_count); 
+
+      uint32_t j(0);  // index into gpu_double_data vector
+      uint32_t y(0);  // index into gpu_long_data vector
+      
+      for (uint32_t gpu = 0; gpu < gpu_count; gpu++)
+      {
+        // Copy the node level fields into the json and set the type field to GPU
+        boost::property_tree::ptree gpu_pt(node_pt);
+        gpu_pt.put(CSM_ENV_DATA_KEY_TYPE, CSM_ENV_DATA_TYPE_GPU);
+
+        for ( uint32_t i = 0; i < gpu_double_labels.size() && j < gpu_double_data.size(); i++ )
+        {
+          gpu_pt.put( gpu_double_labels[i], std::to_string(gpu_double_data[j]) );
+          j++;
+        }       
+        
+        for ( uint32_t x = 0; x < gpu_long_labels.size() && y < gpu_long_data.size(); x++ )
+        {
+          gpu_pt.put( gpu_long_labels[x], std::to_string(gpu_long_data[y]) );
+          y++;
+        } 
+  
+        // Generate the json for this GPU and add it to overall json string
+        std::ostringstream gpu_oss;
+        boost::property_tree::json_parser::write_json(gpu_oss, gpu_pt, false);
+        gpu_oss << std::endl;
+        json += gpu_oss.str();
+      } 
+    }
+  }
+  
+  //LOG( csmenv, debug ) << json;
+ 
+  return json;
 }
 
 CSM_Environmental_Data& CSM_Environmental_Data::operator=( const CSM_Environmental_Data& in )
