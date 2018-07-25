6f5ba79d363f055d3b8f185ad3097c6957e2f066
alda <sanomiya@us.ibm.com>
2018-07-24 22:47:01 -0400

    issue #187: test daxpy-per-socket added

--- hcdiag/src/tests/daxpy/daxpy.sh
+++ hcdiag/src/tests/daxpy/daxpy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash                                                                    
 
 #================================================================================
 #   
@@ -39,6 +39,7 @@ supported_machine
 if [ "$ret" -ne "0" ]; then echo "$me test FAIL, rc=$ret"; exit $ret; fi 
 
 echo "Running daxpy on `hostname -s`, machine type $model."          
+
 
 eye_catcher="PERFORMANCE SUCCESS:"
 trap 'rm -rf /tmp/$$*' EXIT
@@ -81,6 +82,7 @@ read_basics
 
 tmpdir=/tmp/$$
 tmpout=/tmp/$$.out
+tmperr=/tmp/$$.err
 
 # cleanup OS caches
 if [ "$CLEANUP_OS_CACHES" == "1" ]; then
@@ -93,36 +95,81 @@ fi
 if [ ! -x $S_BINDIR/run.daxpy ]; then echo -e "Can not find the daxpy installation.\n$me test FAIL, rc=$rc"; exit 1; fi
 
 mkdir $tmpdir
-echo -e "\nRunning: cd $S_BINDIR; ./run.daxpy -d $tmpdir >$tmpout 2>&1"
-cd $S_BINDIR; ./run.daxpy -d $tmpdir >$tmpout 2>&1
-rc=$?
+level=1
+if [ $# -gt 0 ]; then level=$1; fi 
 
+#### level 1
+rc=0
+if [ $level -eq 1 ]; then
+   echo -e "\nRunning: cd $S_BINDIR; ./run.daxpy -d $tmpdir >$tmpout 2>&1"
+   cd $S_BINDIR; ./run.daxpy -d $tmpdir >$tmpout 2>&1
 
-echo -e "\n================================================================"
-echo -e "\nPrinting daxpy raw output file(s)"
-if [ -d $tmpdir ]; then
-  for file in `ls $tmpdir`; do
-     echo -e "\nFile: $tmpdir/$file"
-     cat $tmpdir/$file
-  done
-fi
-
-echo -e "\n================================================================"
-echo -e "\nResults:\n"
-
-if [ -f $tmpout ]; then
-   cat $tmpout
-   ok=`grep "$eye_catcher" $tmpout`
-   if [ -n "$ok" ]; then
-       echo "$me test PASS, rc=0"
-       exit 0
-   else 
-      rc=2
+   echo -e "\n================================================================"
+   echo -e "\nPrinting daxpy raw output file(s)"
+   if [ -d $tmpdir ]; then
+     for file in `ls $tmpdir`; do
+        echo -e "\nFile: $tmpdir/$file"
+        cat $tmpdir/$file
+     done
    fi
-else
-  rc=3
+   
+   echo -e "\n================================================================"
+   echo -e "\nResults:\n"
+   
+   if [ -f $tmpout ]; then
+      cat $tmpout
+      ok=`grep "$eye_catcher" $tmpout`
+      if [ ! -n "$ok" ]; then rc=2; fi
+   else
+      rc=3
+   fi
+#### level 2
+#### this should go to the run.daxpy (smpi)
+elif [ $level -eq 2 ]; then 
+   ncpu=`grep processor /proc/cpuinfo|wc -l`
+   half=$((ncpu / 2))
+   EXPECTED_PERF=134000
+   re='^[0-9]+([.][0-9]+)?$'
+   if [ $# -gt 1 ]; then EXPECTED_PERF=$2; fi 
+   cd $S_BINDIR
+
+   export GOMP_CPU_AFFINITY="`echo $(seq 0 $((half -1)))`"
+   export OMP_NUM_THREADS=$half
+   for i in 0 8; do
+     if [ $i -eq 8 ]; then export GOMP_CPU_AFFINITY="`echo $(seq $half $((ncpu - 1)))`"; fi
+     cmd="numactl --membind $i ./daxpy -N 100 -M 128 -b 8 > $tmpout 2> $tmperr"
+     echo "Issuing: $cmd"
+     #echo "GOMP_CPU_ACTIVIRY=$GOMP_CPU_AFFINITY"
+     eval ${cmd}
+     if [ -s $tmperr ]; then cat $tmperr; fi 
+     if [ -s $tmpout ]; then cat $tmpout; fi 
+
+     perf=`grep DAXPY $tmpout | awk '{printf $3}'`
+     if  [[ $perf =~ $re ]]; then 
+       perf=`echo ${perf%.*}` 
+       if [[ $perf -lt "$EXPECTED_PERF" ]] ; then
+           echo "$thishost: PERFORMANCE FAILURE: $perf MB/s; EXPECTED PERFORMANCE: $EXPECTED_PERF MB/s "
+           rc=99
+       else
+           echo "$thishost: PERFORMANCE SUCCESS: $perf MB/s; EXPECTED PERFORMANCE: $EXPECTED_PERF MB/s "
+       fi
+     else
+        echo "ERROR parsing the daxpy output file."
+        rc=98
+     fi
+     echo -e "\n\n"
+  done
+else 
+   echo -e "Invalid level $level. Level must be 1 or 2." 
+   rc=1
 fi
 
-echo "$me test FAIL, rc=$rc"
+
+if [ $rc -eq 0 ]; then
+  echo "$me test PASS, rc=$rc"
+else
+  echo "$me test FAIL, rc=$rc"
+fi
 
 exit $rc
+
