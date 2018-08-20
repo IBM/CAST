7872a3811e53eb093e44b6653a998871846ca0e6
John Dunham <jdunham@us.ibm.com>
2018-08-20 17:19:29 -0400

    Fixing a bug in findJobKeys.py that didn't account for Elasticsearch's lack of case sensitivity.

--- csm_big_data/python/findJobKeys.py
+++ csm_big_data/python/findJobKeys.py
@@ -45,7 +45,7 @@ def main(args):
     parser.add_argument( '-t', '--target', metavar='hostname:port', dest='target', default=None, 
         help='An Elasticsearch server to be queried. This defaults to the contents of environment variable "CAST_ELASTIC".')
     parser.add_argument( '-k', '--keywords', metavar='key', dest='keywords', nargs='*', default=['.*'],
-        help='A list of keywords to search for in the Big Data Store (default : .*).')
+        help='A list of keywords to search for in the Big Data Store. Case insensitive regular expressions (default : .*).')
     parser.add_argument( '-v', '--verbose', action='store_true',
         help='Displays any logs that matched the keyword search.' )
     parser.add_argument( '--size', metavar='size', dest='size', default=30,
@@ -144,7 +144,7 @@ def main(args):
     keywords=[]
     should_keywords=[]
     for key in args.keywords:
-        should='{{ "regexp" : {{ "message" : "{0}" }} }}'.format(key)
+        should='{{ "regexp" : {{ "message" : "{0}" }} }}'.format(key.lower())
         filter='"filter" : {0}'.format(should)
         keywords.append('"{0}" : {{ {1} }}'.format(key,filter))
         should_keywords.append(should)
