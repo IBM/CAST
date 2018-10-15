758bfa83ba584cc79b81aecb759a1429c6f6b466
John Dunham <jdunham@us.ibm.com>
2018-10-15 09:21:00 -0400

    csm_db_history_archive.py now handles illegal json characters, reduced default number of threads to 2.

--- csmdb/sql/csm_db_history_archive.py
+++ csmdb/sql/csm_db_history_archive.py
@@ -28,15 +28,15 @@ DEFAULT_TARGET='''/var/log/ibm/csm/archive'''
 DEFAULT_COUNT=1000
 DEFAULT_DATABASE="csmdb"
 DEFAULT_USER="postgres"
-DEFAULT_THREAD_POOL=10
-TABLES=[ "csm_allocation_history" ]
-#TABLES=[ "csm_allocation_history", "csm_allocation_node_history", "csm_allocation_state_history", 
-#    "csm_config_history", "csm_db_schema_version_history", "csm_diag_result_history", 
-#    "csm_diag_run_history", "csm_dimm_history", "csm_gpu_history", "csm_hca_history", 
-#    "csm_ib_cable_history", "csm_lv_history", "csm_lv_update_history", "csm_node_history", 
-#    "csm_node_state_history", "csm_processor_socket_history", "csm_ssd_history", 
-#    "csm_ssd_wear_history", "csm_step_history", "csm_step_node_history", "csm_switch_history",
-#    "csm_switch_inventory_history", "csm_vg_history", "csm_vg_ssd_history"]
+DEFAULT_THREAD_POOL=2
+
+TABLES=[ "csm_allocation_history", "csm_allocation_node_history", "csm_allocation_state_history", 
+    "csm_config_history", "csm_db_schema_version_history", "csm_diag_result_history", 
+    "csm_diag_run_history", "csm_dimm_history", "csm_gpu_history", "csm_hca_history", 
+    "csm_ib_cable_history", "csm_lv_history", "csm_lv_update_history", "csm_node_history", 
+    "csm_node_state_history", "csm_processor_socket_history", "csm_ssd_history", 
+    "csm_ssd_wear_history", "csm_step_history", "csm_step_node_history", "csm_switch_history",
+    "csm_switch_inventory_history", "csm_vg_history", "csm_vg_ssd_history"]
 
 RAS_TABLES=["csm_ras_event_action"]
 
@@ -52,48 +52,52 @@ def dump_table( db, user, table_name, count, target_dir, is_ras=False ):
 
     cursor=db_conn.cursor()
     
-    #cursor.execute("DROP TABLE IF EXISTS temp_{0}".format(table_name))
+    cursor.execute("DROP TABLE IF EXISTS temp_{0}".format(table_name))
 
     ## Make the temp table.
-    #cursor.execute("CREATE TEMP TABLE temp_{0} as \
-    #    ( SELECT *, ctid AS id \
-    #      FROM {0} WHERE archive_history_time IS NULL \
-    #      ORDER BY {1} ASC LIMIT {2} FOR UPDATE)".format(table_name, time, count))
-    #
+    cursor.execute("CREATE TEMP TABLE temp_{0} as \
+        ( SELECT *, ctid AS id \
+          FROM {0} WHERE archive_history_time IS NULL \
+          ORDER BY {1} ASC LIMIT {2} FOR UPDATE)".format(table_name, time, count))
+    
     ## Update the flagged tables.
-    #cursor.execute("UPDATE {0} SET archive_history_time = 'now()' \
-    #    FROM temp_{0} WHERE temp_{0}.id = {0}.ctid".format(table_name))
+    cursor.execute("UPDATE {0} SET archive_history_time = 'now()' \
+        FROM temp_{0} WHERE temp_{0}.id = {0}.ctid".format(table_name))
 
     ## Drop the id from the temp table.
-    #cursor.execute("ALTER TABLE temp_{0} DROP COLUMN id".format(table_name))
+    cursor.execute("ALTER TABLE temp_{0} DROP COLUMN id".format(table_name))
 
     ## Dump the updated table.
-    #cursor.execute("SELECT row_to_json(temp_{0}) FROM temp_{0}".format(table_name))
-    #archive=cursor.fetchall()
-    
+    cursor.execute("SELECT * FROM temp_{0}".format(table_name))
+
     # Build the file name.
     file_name="{0}/{1}.archive.{2}.json".format(target_dir, table_name, date.today())
 
-    cursor.execute( "UPDATE {0} SET archive_history_time = 'now()' \
-        FROM {0} as t\
-        INNER JOIN \
-            (SELECT *,ctid FROM {0} WHERE archive_history_time IS NULL \
-            ORDER BY {1} ASC LIMIT {2})  as filter\
-            ON filter.ctid = t.ctid \
-        RETURNING row_to_json({0})".format(table_name, time, count)) 
+    #cursor.execute( "UPDATE {0} SET archive_history_time = 'now()' \
+    #    FROM {0} as t\
+    #    INNER JOIN \
+    #        (SELECT *,ctid FROM {0} WHERE archive_history_time IS NULL \
+    #        ORDER BY {1} ASC LIMIT {2})  as filter\
+    #        ON filter.ctid = t.ctid \
+    #    RETURNING row_to_json({0})".format(table_name, time, count)) 
     
     # Append the logs to the file.
     try:
         with open(file_name, 'a') as file:
+
+            colnames = [desc[0] for desc in cursor.description]
             for row in cursor:
-                file.write("{}\n".format(json.dumps(row[0])))
+                file.write('{{ "type":"db-{0}", "data":{1} }}\n'.format(
+                    table_name, json.dumps(dict(zip(colnames, row)), default=str)))
     except Exception as e:
-        print e
+        print( "Exception caught: {0}".format(e)
+        cursor.execute("DROP TABLE IF EXISTS temp_{0}".format(table_name))
         db_conn.rollback()
         db_conn.close()
         return
     
     # Commit the records.
+    cursor.execute("DROP TABLE IF EXISTS temp_{0}".format(table_name))
     db_conn.commit()
     db_conn.close()
     
