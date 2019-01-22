3c742694dc91e6aeef8880053381aa80f7272b28
wc morrison <wcmorris@us.ibm.com>
2019-01-22 15:21:37 -0500

    updated Copyright header to 2019

--- csmtest/buckets/basic/db_ras_type_script.sh
+++ csmtest/buckets/basic/db_ras_type_script.sh
@@ -2,7 +2,7 @@
 #   
 #    buckets/basic/db_ras_type_script.sh
 # 
-#  © Copyright IBM Corporation 2015-2018. All Rights Reserved
+#  © Copyright IBM Corporation 2019. All Rights Reserved
 #
 #    This program is licensed under the terms of the Eclipse Public License
 #    v1.0 as published by the Eclipse Foundation and available at
--- csmtest/buckets/basic/db_stats_script.sh
+++ csmtest/buckets/basic/db_stats_script.sh
@@ -1,8 +1,8 @@
 #================================================================================
 #   
-#    buckets/basic/db_stats.sh
+#    buckets/basic/db_stats_script.sh
 # 
-#  © Copyright IBM Corporation 2015-2018. All Rights Reserved
+#  © Copyright IBM Corporation 2019. All Rights Reserved
 #
 #    This program is licensed under the terms of the Eclipse Public License
 #    v1.0 as published by the Eclipse Foundation and available at
