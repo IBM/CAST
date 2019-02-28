8871055d4a502ee6e1a93866fda8dcbeaff9c488
Michael Aho <meaho@us.ibm.com>
2019-02-28 13:23:45 -0500

    Absolute path /tmp for temp file

--- bb/scripts/bb_pre_exec.sh
+++ bb/scripts/bb_pre_exec.sh
@@ -25,8 +25,8 @@ echo "Job $LSF_STAGE_JOBID: $( date ) stage-in process script $( basename $BASH_
 LSF_STAGE_STORAGE_MINSIZE="${LSF_STAGE_STORAGE_MINSIZE}G" #append unit
 export LSF_STAGE_STORAGE_MINSIZE
 export PATH_PRESERVE=$PATH
-export BB_ERR_FILE=`cat /dev/urandom | tr -cd 'a-zA-Z0-9' | head -c 32`
-
+export BB_ERR_FILE="/tmp/"`cat /dev/urandom | tr -cd 'a-zA-Z0-9' | head -c 32`
+echo $BB_ERR_FILE 
 $FLOOR/bb/scripts/stagein_admin.pl 1 | perl -ne 'printf("Job %4d: $_", $ENV{"LSF_STAGE_JOBID"});' &>> /var/log/bb_stagein.log
 su $LSF_STAGE_USER -p -c "$FLOOR/bb/scripts/stagein_admin.pl 2" | perl -ne 'printf("Job %4d: $_", $ENV{"LSF_STAGE_JOBID"});' &>> /var/log/bb_stagein.log
 $FLOOR/bb/scripts/stagein_admin.pl 3 | perl -ne 'printf("Job %4d: $_", $ENV{"LSF_STAGE_JOBID"});' &>> /var/log/bb_stagein.log
