7e62eca551b0beb2d2134d4285e266adb0fa0930
wc morrison <wcmorris@us.ibm.com>
2019-03-05 10:26:18 -0500

    csmdb_updates for 1.6.0: including additional DB checks within some scripts (DB server status and an ERROR output message related to a non root DB user in the system) and also creating a utility script to assist in the PostgreSQL DB auth^Ctication, server connectivity, and user(s) setup.

--- csmdb/sql/csm_db_ras_type_script.sh
+++ csmdb/sql/csm_db_ras_type_script.sh
@@ -16,9 +16,9 @@
 
 #--------------------------------------------------------------------------------
 #   usage:              ./csm_db_ras_type_script.sh
-#   current_version:    01.10
+#   current_version:    01.11
 #   create:             11-07-2017
-#   last modified:      02-15-2019
+#   last modified:      03-04-2019
 #--------------------------------------------------------------------------------
 
 export PGOPTIONS='--client-min-messages=warning'
@@ -77,7 +77,7 @@ LogMsg "[Start   ] Welcome to CSM database ras type automation script."
 LogMsg "${line2_log}"
 echo "[Start   ] Welcome to CSM database ras type automation script."
 echo "${line1_out}"
-echo "[Info    ] Log Dir: $logdir/$logname"
+echo "[Info    ] Log directory: $logdir/$logname"
 
 #----------------------------------------------
 # Usage Command Line Functions
@@ -237,26 +237,40 @@ if [ "$1" != "-l" ] && [ "$1" != "-r" ]; then
      exit 0
 fi
 
+#----------------------------------------------------------------
+# Check the status of the PostgreSQl server
+#----------------------------------------------------------------
+
+systemctl status postgresql > /dev/null
+
+if [ $? -ne 0 ]; then
+    echo "[Error   ] The PostgreSQL server: Is currently inactive and needs to be started"
+    echo "[Info    ] If there is an issue please run: systemctl status postgresql for more info."
+    LogMsg "[Error   ] The PostgreSQL server: Is currently inactive and needs to be started"
+    LogMsg "[Info    ] If there is an issue please run: systemctl status postgresql for more info."
+    LogMsg "${line2_log}"
+    LogMsg "[End     ] Exiting csm_db_ras_type_script.sh script"
+    echo "${line1_out}"
+    echo "${line3_log}" >> $logfile
+    exit 0
+fi
+
 #--------------------------------------------------
 # Check if postgresql exists already and root user
 #--------------------------------------------------
 
-string1="$now1 ($current_user) [Info    ] DB Users:"
-    psql -U $db_username -t -c '\du' | cut -d \| -f 1 | grep -qw root
-        if [ $? -ne 0 ]; then
-            db_user_query=`psql -U $db_username -q -A -t -P format=wrapped <<EOF
-            \set ON_ERROR_STOP true
-            select string_agg(usename,' | ') from pg_user;
-EOF`
-            echo "$string1 $db_user_query" | sed "s/.\{60\}|/&\n$string1 /g" >> $logfile
-            echo "[Error   ] Postgresql may not be configured correctly. Please check configuration settings."
-            LogMsg "[Error   ] Postgresql may not be configured correctly. Please check configuration settings."
-            LogMsg "${line2_log}"
-            LogMsg "[End     ] Exiting csm_db_ras_type_script.sh script"
-            echo "${line1_out}"
-            echo "${line3_log}" >> $logfile
-            exit 0
-        fi
+root_query=`psql -qtA $db_username -c "SELECT usename FROM pg_catalog.pg_user where usename = 'root';" 2>&1`
+    if [ $? -ne 0 ]; then
+        echo "[Error   ] $root_query"
+        echo "$root_query" |& awk '/^psql:.*$/{$1=""; gsub(/^[ \t]+|[ \t]+$/,""); print "'"$(date '+%Y-%m-%d %H:%M:%S') ($current_user) [Error   ] DB Message: "'"$0}' >>"${logfile}"
+        echo "[Info    ] Postgresql may not be configured correctly. Please check configuration settings."
+        LogMsg "[Info    ] Postgresql may not be configured correctly. Please check configuration settings."
+        LogMsg "${line2_log}"
+        LogMsg "[End     ] Exiting csm_db_ras_type_script.sh script"
+        echo "${line1_out}"
+        echo "${line3_log}" >> $logfile
+        exit 0
+    fi
 
 #-------------------------------------------------
 # Check if postgresql exists already and DB name
