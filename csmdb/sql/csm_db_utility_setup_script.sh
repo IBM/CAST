1745024fd8470d1bf0740535bf82b7f67b574b57
wc morrison <wcmorris@us.ibm.com>
2019-03-08 17:16:43 -0500

    copyright fix 2019

--- csmdb/sql/csm_db_utility_setup_script.sh
+++ csmdb/sql/csm_db_utility_setup_script.sh
@@ -3,7 +3,7 @@
 #
 #    csm_db_utility_setup_script.sh
 #
-#  © Copyright IBM Corporation 2018. All Rights Reserved
+#  © Copyright IBM Corporation 2019. All Rights Reserved
 #
 #    This program is licensed under the terms of the Eclipse Public License
 #    v1.0 as published by the Eclipse Foundation and available at
