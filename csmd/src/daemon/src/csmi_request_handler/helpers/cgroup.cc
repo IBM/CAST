38f19e7587072782fc8dbe545492ac067b5516de
John Dunham <jdunham@us.ibm.com>
2019-03-22 11:02:24 -0400

    Fixed off by one error that broke blink disable in cgroup.

--- csmd/src/daemon/src/csmi_request_handler/helpers/cgroup.cc
+++ csmd/src/daemon/src/csmi_request_handler/helpers/cgroup.cc
@@ -1726,12 +1726,12 @@ void CGroup::GetCoreIsolation( int64_t cores, std::string &sysCores, std::string
             {
                 thread =
                      ( socket * coresPerSocket * threadsPerCoreMax ) + (core * threadsPerCoreMax )  ;
-                int32_t extra_thread = thread + + ( threadsPerCoreMax - 1 );
+                int32_t extra_thread = thread + ( threadsPerCoreMax - 1 );
 
                 int32_t cpu = 0; 
                 for(; cpu < threadsPerCore; ++cpu ) sysCores.append(std::to_string(thread++)).append(",");
 
-                for( cpu = threadsPerCoreMax; cpu >= blinkOffset && extra_thread > 0; --cpu ) 
+                for( cpu = threadsPerCoreMax; cpu > blinkOffset && extra_thread > 0; --cpu ) 
                     CPUPower(extra_thread--, CPU_OFFLINE);
             }
         }
