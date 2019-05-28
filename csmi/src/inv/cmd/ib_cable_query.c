7be6e71d56e1690aecc2c2ca99eb782a21f287ba
Nick Buonarota <NickyDaB@gmail.com>
2019-05-28 14:29:07 -0400

    added names to help
    working on https://github.com/IBM/CAST/issues/677

--- csmi/src/inv/cmd/ib_cable_query.c
+++ csmi/src/inv/cmd/ib_cable_query.c
@@ -44,7 +44,7 @@ void help(){
 	puts("_____CSM_IB_CABLE_QUERY_CMD_HELP_____");
 	puts("USAGE:");
 	puts("  csm_ib_cable_query ARGUMENTS [OPTIONS]");
-	puts("  csm_ib_cable_query [-c comments] [-g guids] [-i identifiers] [-l lengths] [-s serial_numbers] [-l limit] [-o offset] [-h] [-v verbose_level]");
+	puts("  csm_ib_cable_query [-c comments] [-g guids] [-i identifiers] [-l lengths] [-n names] [-s serial_numbers] [-l limit] [-o offset] [-h] [-v verbose_level]");
 	puts("");
 	puts("SUMMARY: Used to query the 'csm_ib_cable' table of the CSM database.");
 	puts("");
@@ -61,6 +61,7 @@ void help(){
 	puts("    -g, --guids          | \"248a:0703:006d:40f0,ec0d:9a03:00d3:05b4\" | (STRING) Filter results of the database query to only include cables containing guids in this list. This field will search both 'guid_s1' and 'guid_s2' fields in 'csm_ib_cable' table. ");
 	puts("    -i, --identifiers    | \"QSFP+,Unknown\"                           | (STRING) Filter results of the database query to only include cables containing identifiers in this list. This field uses psql search of 'LIKE'. So add '%' to get partial matches. '%value%' ");
 	puts("    -l, --lengths        | \"2 m,%3%\"                                 | (STRING) Filter results of the database query to only include cables containing lengths in this list. This field uses psql search of 'LIKE'. So add '%' to get partial matches. '%value%'. ");
+	puts("    -n, --names          | \"%248a0703006d40f0_14%\"                   | (STRING) Filter results of the database query to only include cables containing names in this list. This field uses psql search of 'LIKE'. So add '%' to get partial matches. '%value%' ");
 	
 
 	puts("    -s, --serial_numbers | \"abc123,xyz789\"                           | (STRING) This is a csv field of serial numbers to query. Filter results to only include records that have a matching serial numbers. The serial number is a unique identification for an ib cable.");
