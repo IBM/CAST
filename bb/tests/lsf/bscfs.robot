a59f42a358684262beda322cfd828a13699e6ba8
Tom Gooding <tgooding@us.ibm.com>
2019-07-19 15:32:56 -0500

    Add BSCFS write to SSD and block for xfer testcase

--- bb/tests/lsf/bscfs.robot
+++ bb/tests/lsf/bscfs.robot
@@ -17,9 +17,33 @@ bscfs access check
       [Timeout]  5 minutes
       Using SSD  16
       Using bscfs
-      set num computes  2
+      ${maxnodes} =  Run  /opt/ibm/csm/bin/csm_node_resources_query_all | grep IN_SERVICE | wc -l
+      Set num computes  ${maxnodes}
       bsub&wait  ${jsrun} ${WORKDIR}/bb/tests/bin/accesscheck.sh  0  5mins
 
+bscfs blocking write single-node
+	 [Tags]  lsf  bscfs
+   	 [Timeout]  15 minutes
+	 Using SSD  256
+	 Using bscfs
+	 
+	 bscfs cleanup	 
+	 bsub&wait  ${jsrun} ${WORKDIR}/bscfs/tests/chkpnt_write_blocking --chkpnt_count 2 --chkpnt_size 16G --chunk_size 16M --chkpnt_dir . --keep_all  0  15mins
+	 
+	 bscfs cleanup
+
+bscfs blocking write multi-node
+	 [Tags]  lsf  bscfs
+   	 [Timeout]  15 minutes
+	 Using SSD  256
+	 Using bscfs
+     ${maxnodes} =  Run  /opt/ibm/csm/bin/csm_node_resources_query_all | grep IN_SERVICE | wc -l
+	 Set num computes  ${maxnodes}
+	 
+	 bscfs cleanup	 
+	 bsub&wait  ${jsrun} ${WORKDIR}/bscfs/tests/chkpnt_write_blocking --chkpnt_count 2 --chkpnt_size 16G --chunk_size 16M --chkpnt_dir . --keep_all  0  15mins
+	 
+	 bscfs cleanup
 
 bscfs checkpoint write series single-node
 	 [Tags]  lsf  bscfs
@@ -38,7 +62,8 @@ bscfs checkpoint write series multi-server
    	 [Timeout]  30 minutes
 	 Using SSD  256
 	 Using bscfs
-	 Set num computes  2
+     ${maxnodes} =  Run  /opt/ibm/csm/bin/csm_node_resources_query_all | grep IN_SERVICE | wc -l
+	 Set num computes  ${maxnodes}
 	 
 	 bscfs cleanup	 
 	 bsub&wait  ${jsrun} ${WORKDIR}/bscfs/tests/chkpnt_write --compute_time 40 --chkpnt_count 5 --chkpnt_size 1G --chunk_size 256M --chkpnt_dir . --keep_all  0  30mins
@@ -65,7 +90,8 @@ bscfs checkpoint read series multi-server
       [Timeout]  30 minutes
       Using SSD  256
       Using bscfs
-      Set num computes  2
+      ${maxnodes} =  Run  /opt/ibm/csm/bin/csm_node_resources_query_all | grep IN_SERVICE | wc -l
+	  Set num computes  ${maxnodes}
 
       bscfs cleanup
       bsub&wait  ${jsrun} ${WORKDIR}/bscfs/tests/chkpnt_write --compute_time 40 --chkpnt_count 5 --header_size 4K --chkpnt_size 1G --stripe_size 512M --chkpnt_dir . --keep_all  0  30mins
@@ -91,7 +117,8 @@ bscfs checkpoint write every kth checkpoint multi-server
       [Timeout]  30 minutes
       Using SSD  256
       Using bscfs
-      Set num computes  2
+      ${maxnodes} =  Run  /opt/ibm/csm/bin/csm_node_resources_query_all | grep IN_SERVICE | wc -l
+	  Set num computes  ${maxnodes}
       
       bscfs cleanup
       bsub&wait  ${jsrun} ${WORKDIR}/bscfs/tests/chkpnt_combo -compute_time 40 --chkpnt_count 12 --chkpnt_start 0 --primary_interval 3 --header_size 4K --chkpnt_size 6G --stripe_size 512M --chkpnt_dir .  0  30mins
---
+++ bscfs/tests/chkpnt_write_blocking.c
@@ -0,0 +1,149 @@
+/******************************************************************************
+ |    chkpnt_write.c
+ |
+ |  © Copyright IBM Corporation 2016. All Rights Reserved
+ |
+ |    This program is licensed under the terms of the Eclipse Public License
+ |    v1.0 as published by the Eclipse Foundation and available at
+ |    http://www.eclipse.org/legal/epl-v10.html
+ |
+ |    U.S. Government Users Restricted Rights:  Use, duplication or disclosure
+ |    restricted by GSA ADP Schedule Contract with IBM Corp.
+ ******************************************************************************/
+
+#include "chkpnt_common.h"
+#include "bscfsAPI.h"
+
+int main(int argc, char **argv)
+{
+    chkpnt_init(argc, argv);
+
+    if (Rank == 0) 
+    {
+    	printf("Initializing.\n");
+    }
+
+    struct timespec timer;
+    clock_gettime(CLOCK_REALTIME, &timer);
+
+    int rc;
+
+    uint64_t mem_size = MemSize(Rank);
+    uint64_t stripe_count = (mem_size + StripeSize - 1) / StripeSize;
+
+    void *memory, *header;
+    rc = posix_memalign(&memory, 0x10000, mem_size);
+    Check(rc == 0, "posix_memalign(memory) failed", NULL, rc);
+    header = NULL;
+    if (HeaderSize > 0) 
+    {
+        rc = posix_memalign(&header, 0x10000, HeaderSize);
+        Check(rc == 0, "posix_memalign(header) failed", NULL, rc);
+    }
+
+    BBTransferHandle_t handle = -1;
+
+    int n;
+    for (n = ChkpntStart; n < (ChkpntStart + ChkpntCount); n++) 
+    {
+        void *mem_start;
+        uint64_t file_start, stripe_size, size, offset, i;
+
+        // "compute" for the specified number of seconds
+        Print(&timer, "Starting computation round %d.\n", n);
+        struct timespec time0;
+        clock_gettime(CLOCK_REALTIME, &time0);
+
+        // fill memory stripes as part of the computation
+        for (i = 0; i < stripe_count; i++) 
+        {
+            mem_start = memory + (i * StripeSize);
+            file_start = HeaderSize + (((i * RankCount) + Rank) * StripeSize);
+            stripe_size = Min(StripeSize, mem_size - ((uint64_t) mem_start));
+            for (offset = 0; offset < stripe_size; offset += 8) {
+            // each word contains its own file offset and the chkpnt number
+            *((uint64_t *) (mem_start + offset)) =
+                (((uint64_t) n) << 56) | (file_start + offset);
+            }
+        }
+        // fill the header if rank 0
+        if ((Rank == 0) && (HeaderSize > 0)) {
+            ((uint64_t *) header)[0] = n;
+            for (offset = 8; offset < HeaderSize; offset += 8) {
+            *((uint64_t *) (header + offset)) = -1ul;
+            }
+        }
+
+
+        Print(&timer, "Writing checkpoint %d.\n", n);
+        struct timespec bw_timer = timer;
+        int fd = open(Chkpnt(n), O_WRONLY|O_CREAT, 0640);
+        Check(fd >= 0, "open(%s) failed", Chkpnt(n), errno);
+        if ((Rank == 0) && (HeaderSize > 0)) {
+            rc = pwrite(fd, header, HeaderSize, 0);
+            Check(rc == HeaderSize, "pwrite(%s header) failed", Chkpnt(n), 0);
+        }
+        for (i = 0; i < stripe_count; i++) {
+            mem_start = memory + (i * StripeSize);
+            file_start = HeaderSize + (((i * RankCount) + Rank) * StripeSize);
+            stripe_size = Min(StripeSize, mem_size - ((uint64_t) mem_start));
+            for (offset = 0; offset < stripe_size; offset += ChunkSize) {
+            size = Min(ChunkSize, stripe_size - offset);
+            rc = pwrite(fd, mem_start + offset, size, file_start + offset);
+            Check(rc >= 0, "pwrite(%s) failed", Chkpnt(n), errno);
+            Check(rc == size, "pwrite(%s) truncated", Chkpnt(n), 0);
+            }
+        }
+        rc = fsync(fd);
+        Check(rc == 0, "fsync(%s) failed", Chkpnt(n), errno);
+        rc = close(fd);
+        Check(rc == 0, "close(%s) failed", Chkpnt(n), errno);
+
+        Print(&timer, "Checkpoint %d write to SSD complete:\n", n);
+        MPI_Barrier(MPI_COMM_WORLD);
+
+        PrintBandwidth(&timer, &bw_timer);
+        bw_timer = timer;
+
+        // start transferring the current checkpoint
+        Print(NULL, "Starting checkpoint %d transfer.\n", n);
+        rc = BSCFS_StartLocalFlush(Chkpnt(n), Mapfile(n), Cleanup(), &handle);
+        Check(rc == 0, "BSCFS_StartLocalFlush(%s) failed", Chkpnt(n), rc);
+
+        // wait for the transfer of the previous checkpoint to complete
+        Print(&timer, "Awaiting checkpoint %d local transfer.\n", n);
+        rc = BSCFS_AwaitLocalTransfer(handle);
+        Check(rc == 0, "BSCFS_AwaitLocalTransfer(%s) failed",
+                            Chkpnt(n), rc);
+
+        Print(&timer, "Checkpoint %d transfer to PFS complete:\n", n);
+        MPI_Barrier(MPI_COMM_WORLD);
+        PrintBandwidth(&timer, &bw_timer);
+        
+        // the previous checkpoint has been transferred by all nodes
+        Print(&timer, "Finalizing checkpoint %d.\n", n);
+        rc = BSCFS_GlobalFlushCompleted(Chkpnt(n));
+        Check(rc == 0, "BSCFS_GlobalFlushCompleted(%s) failed",
+                            Chkpnt(n), rc);
+        
+        rc = BSCFS_Forget(Chkpnt(n));
+        Check(rc == 0, "BSCFS_Forget(%s) failed", Chkpnt(n), errno);
+        
+        if (Rank == 0) 
+        {
+            // delete superseded checkpoint
+            rc = unlink(ChkpntPFS(n));
+            Check(rc == 0, "unlink(%s) failed", ChkpntPFS(n), errno);
+            rc = unlink(Mapfile(n));
+            Check(rc == 0, "unlink(%s) failed", Mapfile(n), errno);
+        }
+        Print(&timer, "Barrier.\n", 0);
+        MPI_Barrier(MPI_COMM_WORLD);
+    }
+    
+    // final checkpoint will be documented by cleanup script on the FEN
+
+    MPI_Finalize();
+    Print(&timer, "Finished.\n", 0);
+    return 0;
+}
