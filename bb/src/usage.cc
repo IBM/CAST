946b367758dcd6a581e3ad06cf06e0048adfd1a1
David Hermsmeier <dlherms@us.ibm.com>
2020-03-12 11:40:08 -0400

    bb:For asyncRemoveJobInfo(), for a given interval, do not kick out to next interval simply because the IB activity is too high at the time when starting to iterate through the ended jobs

--- bb/src/usage.cc
+++ bb/src/usage.cc
@@ -689,109 +689,102 @@ void* asyncRemoveJobInfo(void* ptr)
                     int rc = HandleFile::get_xbbServerGetCurrentJobIds(l_PathJobIds, ONLY_RETURN_REMOVED_JOBIDS);
                     if ((!rc) && l_PathJobIds.size() > 0)
                     {
-                        if (highIB_Activity() >= 0)
+                        for (size_t i=0; i<l_PathJobIds.size(); i++)
                         {
-                            for (size_t i=0; i<l_PathJobIds.size(); i++)
+                            bfs::path job = bfs::path(l_PathJobIds[i]);
+                            bfs::path l_PathToRemove = job.parent_path().string() + "/." + job.filename().string();
+                            LOG(bb,debug) << "asyncRemoveJobInfo(): " << job.string() << " being renamed to " << l_PathToRemove.string();
+                            try
                             {
-                                bfs::path job = bfs::path(l_PathJobIds[i]);
-                                bfs::path l_PathToRemove = job.parent_path().string() + "/." + job.filename().string();
-                                LOG(bb,debug) << "asyncRemoveJobInfo(): " << job.string() << " being renamed to " << l_PathToRemove.string();
+                                bfs::rename(job, l_PathToRemove);
+                            }
+                            catch (std::exception& e1)
+                            {
+                                rc = -1;
+                            }
+
+                            if (!rc)
+                            {
                                 try
                                 {
-                                    bfs::rename(job, l_PathToRemove);
-                                }
-                                catch (std::exception& e1)
-                                {
-                                    rc = -1;
-                                }
-
-                                if (!rc)
-                                {
-                                    try
+                                    LOG(bb,info) << "asyncRemoveJobInfo(): START: Removal of cross-bbServer metadata at " << l_PathJobIds[i];
+                                    for (auto& jobstep : boost::make_iterator_range(bfs::directory_iterator(l_PathToRemove), {}))
                                     {
-                                        LOG(bb,info) << "asyncRemoveJobInfo(): START: Removal of cross-bbServer metadata at " << l_PathJobIds[i];
-                                        for (auto& jobstep : boost::make_iterator_range(bfs::directory_iterator(l_PathToRemove), {}))
+                                        if (!pathIsDirectory(jobstep)) continue;
+                                        for (auto& handlebucket : boost::make_iterator_range(bfs::directory_iterator(jobstep), {}))
                                         {
-                                            if (!pathIsDirectory(jobstep)) continue;
-                                            for (auto& handlebucket : boost::make_iterator_range(bfs::directory_iterator(jobstep), {}))
+                                            if (!pathIsDirectory(handlebucket)) continue;
+                                            for (auto& handledir : boost::make_iterator_range(bfs::directory_iterator(handlebucket), {}))
                                             {
-                                                if (!pathIsDirectory(handlebucket)) continue;
-                                                for (auto& handledir : boost::make_iterator_range(bfs::directory_iterator(handlebucket), {}))
+                                                if (!pathIsDirectory(handledir)) continue;
+                                                string l_HandleDir = handledir.path().string();
+                                                bool l_AllDone = false;
+                                                while (!l_AllDone)
                                                 {
-                                                    if (!pathIsDirectory(handledir)) continue;
-                                                    string l_HandleDir = handledir.path().string();
-                                                    bool l_AllDone = false;
-                                                    while (!l_AllDone)
+                                                    l_AllDone = true;
+                                                    if (!highIB_Activity())
                                                     {
-                                                        l_AllDone = true;
-                                                        if (!highIB_Activity())
+                                                        try
                                                         {
-                                                            try
-                                                            {
-                                                                bfs::remove_all(handledir);
-                                                                LOG(bb,debug) << "asyncRemoveJobInfo():   END: Successful prune of cross-bbServer metadata at " << l_HandleDir;
-                                                            }
-                                                            catch (std::exception& e1)
-                                                            {
-                                                                LOG(bb,warning) << "asyncRemoveJobInfo():   END: Unsuccessful prune of cross-bbServer metadata at " << l_HandleDir << ". Processing will continue...";
-                                                            }
+                                                            bfs::remove_all(handledir);
+                                                            LOG(bb,debug) << "asyncRemoveJobInfo():   END: Successful prune of cross-bbServer metadata at " << l_HandleDir;
+                                                        }
+                                                        catch (std::exception& e1)
+                                                        {
+                                                            LOG(bb,warning) << "asyncRemoveJobInfo():   END: Unsuccessful prune of cross-bbServer metadata at " << l_HandleDir << ". Processing will continue...";
+                                                        }
 
-                                                        }
-                                                        else
-                                                        {
-                                                            l_AllDone = false;
-                                                            LOG(bb,info) << "asyncRemoveJobInfo(): IB activity too high to do prune at " << l_HandleDir \
-                                                                         << ". Current " << g_IB_Adapter << " port_rcv_data delta " << g_Last_Port_Rcv_Data_Delta \
-                                                                         << ", current " << g_IB_Adapter << " port_xmit_data delta " << g_Last_Port_Xmit_Data_Delta \
-                                                                         << ", current IB stats low activity clip value " << g_IBStatsLowActivityClipValue \
-                                                                         << ". Delaying " << g_DiskStatsRate << " seconds...";
-                                                            sleep(g_DiskStatsRate);
-                                                        }
+                                                    }
+                                                    else
+                                                    {
+                                                        l_AllDone = false;
+                                                        LOG(bb,info) << "asyncRemoveJobInfo(): IB activity too high to do prune at " << l_HandleDir \
+                                                                     << ". Current " << g_IB_Adapter << " port_rcv_data delta " << g_Last_Port_Rcv_Data_Delta \
+                                                                     << ", current " << g_IB_Adapter << " port_xmit_data delta " << g_Last_Port_Xmit_Data_Delta \
+                                                                     << ", current IB stats low activity clip value " << g_IBStatsLowActivityClipValue \
+                                                                     << ". Delaying " << g_DiskStatsRate << " seconds...";
+                                                        sleep(g_DiskStatsRate);
                                                     }
                                                 }
                                             }
                                         }
-                                        bool l_AllDone = false;
-                                        while (!l_AllDone)
+                                    }
+                                    bool l_AllDone = false;
+                                    while (!l_AllDone)
+                                    {
+                                        l_AllDone = true;
+                                        if (!highIB_Activity())
                                         {
-                                            l_AllDone = true;
-                                            if (!highIB_Activity())
+                                            try
                                             {
-                                                try
-                                                {
-                                                    bfs::remove_all(l_PathToRemove);
-                                                    LOG(bb,info) << "asyncRemoveJobInfo():   END: Successful removal of cross-bbServer metadata at " << l_PathJobIds[i];
-                                                }
-                                                catch (std::exception& e2)
-                                                {
-                                                    LOG(bb,error) << "asyncRemoveJobInfo():   END: Unsuccessful removal of cross-bbServer metadata at " << l_PathJobIds[i] \
-                                                                  << ". This cross-bbServer metadata must be manually removed.";
-                                                }
+                                                bfs::remove_all(l_PathToRemove);
+                                                LOG(bb,info) << "asyncRemoveJobInfo():   END: Successful removal of cross-bbServer metadata at " << l_PathJobIds[i];
                                             }
-                                            else
+                                            catch (std::exception& e2)
                                             {
-                                                l_AllDone = false;
-                                                LOG(bb,info) << "asyncRemoveJobInfo(): IB activity too high to do final removal at " << l_PathJobIds[i] \
-                                                             << ". Current " << g_IB_Adapter << " port_rcv_data delta " << g_Last_Port_Rcv_Data_Delta \
-                                                             << ", current " << g_IB_Adapter << " port_xmit_data delta " << g_Last_Port_Xmit_Data_Delta \
-                                                             << ", current IB stats low activity clip value " << g_IBStatsLowActivityClipValue \
-                                                             << ". Delaying " << g_DiskStatsRate << " seconds...";
-                                                sleep(g_DiskStatsRate);
+                                                LOG(bb,error) << "asyncRemoveJobInfo():   END: Unsuccessful removal of cross-bbServer metadata at " << l_PathJobIds[i] \
+                                                              << ". This cross-bbServer metadata must be manually removed.";
                                             }
                                         }
-                                    }
-                                    catch (std::exception& e3)
-                                    {
-                                        LOG_ERROR_WITH_EXCEPTION(__FILE__, __FUNCTION__, __LINE__, e3);
-                                        continue;
+                                        else
+                                        {
+                                            l_AllDone = false;
+                                            LOG(bb,info) << "asyncRemoveJobInfo(): IB activity too high to do final removal at " << l_PathJobIds[i] \
+                                                         << ". Current " << g_IB_Adapter << " port_rcv_data delta " << g_Last_Port_Rcv_Data_Delta \
+                                                         << ", current " << g_IB_Adapter << " port_xmit_data delta " << g_Last_Port_Xmit_Data_Delta \
+                                                         << ", current IB stats low activity clip value " << g_IBStatsLowActivityClipValue \
+                                                         << ". Delaying " << g_DiskStatsRate << " seconds...";
+                                            sleep(g_DiskStatsRate);
+                                        }
                                     }
                                 }
-                                rc = 0;
+                                catch (std::exception& e3)
+                                {
+                                    LOG_ERROR_WITH_EXCEPTION(__FILE__, __FUNCTION__, __LINE__, e3);
+                                    continue;
+                                }
                             }
-                        }
-                        else
-                        {
-                            LOG(bb,info) << "asyncRemoveJobInfo(): No removal of cross-bbServer metadata will be attempted during this interval as the IB activity is undetermined";
+                            rc = 0;
                         }
                     }
                     BB_GetTime(l_Time);
